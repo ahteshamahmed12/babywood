65a05223734817d1446b3488891e9cb6bbeeee82
devil-coder-666 <sweasubhan89@gmail.com>
2025-01-29 23:15:52 +0500

    subhan create navbar

---
+++ src/app/Components/Navbar.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image"
+import { ShoppingBag } from 'lucide-react';
+import { Menu } from 'lucide-react';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+  
+function Navbar() {
+    const links = ["HOME","PRODUCTS","CONTACT"]
+  return (
+    <>
+    <div className="w-full h-[100px] bg-[#8BD5FF] flex justify-between items-center px-8 ">
+        <div>
+            <Image src={"/babyWood.png"} alt="logo " height={100} width={100}/>
+        </div>
+        <div  className="hidden md:block">
+            <ul className="flex gap-12" >
+                {links.map((e,i)=>{
+                    return(
+                        <li className="text-[#24224F] font-medium" key={i}>{e}</li>
+                    )
+                })}
+                </ul>
+        </div>
+        <div className=" flex">
+                <div className="pr-4 pt-1  " >
+                    <ShoppingBag size={30} className="text-[#24224F]"/>   
+                </div>
+                <button className=" hidden md:block bg-white px-5 py-1 text-[#24224F] font-medium rounded-[3px]">LOGIN</button>
+                <div className="md:hidden ">
+        <DropdownMenu >
+  <DropdownMenuTrigger  aria-setsize={35} ><Menu size={35}/></DropdownMenuTrigger>
+  <DropdownMenuContent>
+    
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>HOME</DropdownMenuItem>
+    <DropdownMenuItem>PRODUCTS</DropdownMenuItem>
+    <DropdownMenuItem>CONTACT</DropdownMenuItem>
+    <DropdownMenuItem >LOGIN</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+
+        </div>
+        </div>
+       
+    </div>
+    </>
+  )
+}
+
+export default Navbar
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
+
 import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
 
 export default function Home() {
   return (
    <div>
+    <div>
+      <Navbar />
+    </div>
+    
     <Footer/>
+
    </div>
   );
 }
